1065fb960c9ec95815b9127b2fe210dfc2eab725
ahstein3521 <adamhs3521@gmail.com>
2019-03-03 09:30:57 -0500

    added loading indicator and buttons to card

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,42 +1,52 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 // import CardMedia from '@material-ui/core/CardMedia';
 // import Button from '@material-ui/core/Button';
 // import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const App = props => {
-
-  if (!props.name) return null;
 
   return (
 
     <Card style={{margin: 5}}>
-      <CardActionArea>
-        <CardContent>
-          <h1 style={{textAlign: 'center'}}>{ props.main.temp }&deg;F  </h1>
-          <h2> { props.name }</h2>  
-           {/* <h4>{ props.coord.lon} &deg; , { props.coord.lat } &deg;</h4>*/}
-          <div style={{display:'flex'}}>
-          <h3> { props.weather[0].description } </h3>
-          <img 
-            alt="weather icon"
-            style={{verticalAlign:'middle'}} 
-            src={"http://openweathermap.org/img/w/" + props.weather[0].icon + ".png"}
-          />
-         </div>
-        </CardContent>
-      </CardActionArea>
-      {/*<CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>*/}
+      {
+        props.name ?
+        <CardActionArea>
+          <CardContent>
+            <h1 style={{textAlign: 'center'}}>{ props.main.temp }&deg;F  </h1>
+            <h2> { props.name }</h2>  
+             {/* <h4>{ props.coord.lon} &deg; , { props.coord.lat } &deg;</h4>*/}
+            <div style={{display:'flex'}}>
+            <h3> { props.weather[0].description } </h3>
+            <img 
+              alt="weather icon"
+              style={{verticalAlign:'middle'}} 
+              src={"http://openweathermap.org/img/w/" + props.weather[0].icon + ".png"}
+            />
+           </div>
+          </CardContent>
+        </CardActionArea>
+        :
+         <CircularProgress />
+      }
+      <CardActions>
+        <label htmlFor="icon-button-file">
+          <IconButton color="primary" component="span">
+            <RefreshIcon />
+          </IconButton>
+        </label>
+        <IconButton aria-label="Delete">
+          <DeleteIcon />
+        </IconButton>        
+      </CardActions>
     </Card>
   )
 }
